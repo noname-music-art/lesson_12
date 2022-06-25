421049d3a5109f15f2ab2b0fa8a46606539f2b35
Konstantin <vasilevki@gmail.com>
2022-06-25 13:00:02 +0300

    Solution from tutor

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
+import logging
+from json import JSONDecodeError
+
 from flask import Blueprint, render_template, request
+
+from functions import get_posts_by_word
 
 main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
 
@@ -10,4 +15,13 @@ def main_page():
 
 @main_blueprint.route('/search/')
 def search_page():
-    search_query = request.arg.get('s', '')
+    search_query = request.args.get('s', '')
+    logging.info('Поиск')
+    try:
+        posts = get_posts_by_word(search_query)
+    except FileNotFoundError:
+        logging.error('Файл не найден')
+        return 'Файл не найден'
+    except JSONDecodeError:
+        return 'Алярма'
+    return render_template('post_list.html', query=search_query, posts=posts)
