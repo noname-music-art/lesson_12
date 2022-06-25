0ac536ffb1544f796e55f593bcc9c0392cdc4105
noname_art <vasilevki@gmail.com>
2022-06-25 19:11:04 +0300

    Comments added

--- functions.py
+++ functions.py
@@ -2,11 +2,13 @@ import json
 
 
 def load_posts() -> list[dict]:
+    """Load posts from JSON file"""
     with open('posts.json', 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def get_posts_by_word(word: str) -> list[dict]:
+    """Search post by keyword"""
     result = []
     for post in load_posts():
         if word.lower() in post['content'].lower():
@@ -15,6 +17,7 @@ def get_posts_by_word(word: str) -> list[dict]:
 
 
 def add_post(post: dict) -> dict:
+    """Add new post to JSON file"""
     posts: list[dict] = load_posts()
     posts.append(post)
     with open('posts.json', 'w', encoding='utf-8') as file:
